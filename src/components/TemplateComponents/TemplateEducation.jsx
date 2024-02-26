24fd4e683ac5701ff8ca1f9450a0c2a5973f65b4
sachinnautiyal-github <nautiyalsachin217@gmail.com>
2024-02-26 19:43:25 +0530

    changes

--- src/components/TemplateComponents/TemplateEducation.jsx
+++ src/components/TemplateComponents/TemplateEducation.jsx
@@ -1,19 +1,15 @@
-// import "../Styles/TemplateEducation.css";
+
 import React from 'react'
 
 const TemplateEducation = (props) => {
   return (
-    <>
+    <> 
+      <div>
       <p className="template-education-details">
-      <p  style={{fontSize:'15px', fontWeight:'600'}}>{props.education.degree} </p>
-      {props.education.domain}</p>   
-      <p className="template-education-university">
-        {props.education.university}
-      </p>
-       <p className="education-start-end">
-        ({props.education.startYear} - {props.education.endYear})
-      </p>
-    
+      <p  style={{fontSize:'15px', fontWeight:'600'}}>{props.education.degree} </p>{props.education.domain}</p>   
+      <p className="template-education-university"> {props.education.university} </p>
+       <p className="education-start-end"> ({props.education.startYear} - {props.education.endYear}) </p>
+       </div>
     </>
     
   )
